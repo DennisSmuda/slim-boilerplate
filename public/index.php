79abd8a549c8f344d7e2a5a2419f450786429f93
Dennis Smuda <dennissmuda@msn.com>
2016-05-17 20:37:19 +0200

    initial commit

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php
+if (PHP_SAPI == 'cli-server') {
+    // To help the built-in PHP dev server, check if the request was actually for
+    // something which should probably be served as a static file
+    $file = __DIR__ . $_SERVER['REQUEST_URI'];
+    if (is_file($file)) {
+        return false;
+    }
+}
+
+require __DIR__ . '/../vendor/autoload.php';
+
+session_start();
+
+// Instantiate the app
+$settings = require __DIR__ . '/../src/settings.php';
+$app = new \Slim\App($settings);
+
+// Set up dependencies
+require __DIR__ . '/../src/dependencies.php';
+
+// Register middleware
+require __DIR__ . '/../src/middleware.php';
+
+// Twig Container for Application
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig('./templates/', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $basePath = rtrim(str_ireplace('index.php', '', $c['request']->getUri()->getBasePath()), '/');
+    $view->addExtension(new Slim\Views\TwigExtension($c['router'], $basePath));
+
+    return $view;
+};
+
+$app->get('/', function($request, $response, $args) {
+  return $this->view->render($response, 'index.twig');
+});
+
+// Define named route
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    return $this->view->render($response, 'profile.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
+
+// Run app
+$app->run();
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+
+module.exports = {
+  resolve: {
+    modulesDirectories: ['node_modules', 'scripts', 'stylesheets'],
+    extensions: ['', '.js', '.scss'],
+  },
+  entry: ['./assets/js/index.js', './assets/styles/main.scss'],
+  output: {
+    filename: './assets/dist/browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('css-loader?sourceMap!sass-loader?sourceMap=true&sourceMapContents=true')
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("./assets/dist/main.css")
+  ]
+};
